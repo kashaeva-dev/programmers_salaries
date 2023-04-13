a1fe6de3710b859ba18d62c7834fd36b7e8e1f7d
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-04-13 14:13:34 +0300

    Add salary_table_output() function to print results as a AsciiTable

--- get_salaries.py
+++ get_salaries.py
@@ -8,7 +8,7 @@ from fetch_vacancies import fetch_vacancies_hh, fetch_vacancies_sj
 from predict_salary import predict_salary_hh, predict_salary_sj
 
 
-def get_salaries_by_languages(languages, service):
+def get_salaries_by_languages(languages, service, token=None):
     job_search_services = {
         'hh': {
             'fetch': fetch_vacancies_hh,
@@ -22,18 +22,50 @@ def get_salaries_by_languages(languages, service):
     salaries_by_language = []
 
     for language in languages:
-        vacancies = job_search_services[service]['fetch'](language)
+        vacancies = job_search_services[service]['fetch'](language, token)
         salaries = []
         for vacancy in vacancies['vacancies']:
             salary = job_search_services[service]['predict'](vacancy)
             if salary:
                 salaries.append(salary)
-        salaries_by_language.append([
-            language,
-            vacancies['found'],
-            len(salaries),
-            int(mean(salaries))
-        ])
+        if salaries:
+            salaries_by_language.append([
+                language,
+                vacancies['found'],
+                len(salaries),
+                int(mean(salaries))
+            ])
+        else:
+            salaries_by_language.append([
+                language,
+                vacancies['found'],
+                len(salaries),
+                'Неизвестно',
+            ])
 
     return salaries_by_language
 
+
+def salary_table_output(table_data, table_title):
+    headings = [[
+        'Язык программирования',
+        'Вакансий найдено',
+        'Вакансий обработано',
+        'Средняя зарплата',
+    ]]
+    table_data = headings + table_data
+    table_instance = AsciiTable(table_data, table_title)
+    print(table_instance.table)
+    print()
+
+
+if __name__ == "__main__":
+    load_dotenv(find_dotenv())
+    token = os.environ['SUPERJOB_KEY']
+
+    languages = [
+        'JavaScript', 'Java', 'Python', 'Ruby', 'PHP',
+        'C++', 'C', 'Go', 'Swift', 'TypeScript', '1C',
+    ]
+
+    salary_table_output(get_salaries_by_languages(languages, 'sj', token), 'SuperJob Moscow')
